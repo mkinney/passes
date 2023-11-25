fd6aca362d15c4c582dca8750c71fe56f009220b
Mike Kinney <mike.kinney@gmail.com>
2023-11-24 21:16:07 -0800

    revert

--- gear_cutting.ino
+++ gear_cutting.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <AccelStepper.h>
+//#include <AccelStepper.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27, if new version please use 0x3F instead.
 
@@ -16,11 +16,12 @@ const int key_delay = 200;
 int tooth_num = 1;
 int total_teeth = 10;
 
-long x = 100;
+int x;
+//long x = 100;
 
 // stepper
 //AccelStepper stepper1(AccelStepper::DRIVER, 9, 8);
-AccelStepper stepper1(AccelStepper::FULL2WIRE, 9, 8);
+//AccelStepper stepper1(AccelStepper::FULL2WIRE, 9, 8);
 
 
 void setup() {
@@ -34,8 +35,12 @@ void setup() {
   pinMode(PIN_K3, INPUT_PULLUP);
   pinMode(PIN_K4, INPUT_PULLUP);
 
-  stepper1.setMaxSpeed(200);
-  stepper1.setAcceleration(50);
+  //stepper1.setMaxSpeed(200);
+  //stepper1.setAcceleration(50);
+
+  // stepper
+  pinMode(9,OUTPUT); // set Pin9 as PUL
+  pinMode(8,OUTPUT); // set Pin8 as DIR
 
   show();
 
@@ -89,10 +94,27 @@ void loop() {
     show();
     delay(key_delay);
 
-    x += 100;
+    digitalWrite(8,HIGH); // set high level direction
+    for(x = 0; x < 400; x++) {
+      digitalWrite(9,HIGH); // Output high
+      delayMicroseconds(500); // set rotate speed
+      digitalWrite(9,LOW); // Output low
+      delayMicroseconds(500); // set rotate speed
+    }
+    delay(1000); //pause 1 second
+    digitalWrite(8,LOW); // set high level direction
+    for(x = 0; x < 400; x++) {
+      digitalWrite(9,HIGH);
+      delayMicroseconds(500);
+      digitalWrite(9,LOW);
+      delayMicroseconds(500);
+    }
+    delay(1000);
+
+    //x += 100;
     //x = (tooth_num / total_teeth * 360);
-    stepper1.moveTo(x);
-    stepper1.run();
+    //stepper1.moveTo(x);
+    //stepper1.run();
   }
 
 } // loop
