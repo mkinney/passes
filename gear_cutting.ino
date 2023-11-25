3339b378cc4bf1da3c2c256462148c6c3f62d0a0
Mike Kinney <mike.kinney@gmail.com>
2023-11-24 22:09:35 -0800

    kinda working

--- gear_cutting.ino
+++ gear_cutting.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-//#include <AccelStepper.h>
+#include <AccelStepper.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27, if new version please use 0x3F instead.
 
@@ -16,13 +16,12 @@ const int key_delay = 200;
 int tooth_num = 1;
 int total_teeth = 10;
 
-int x;
-//long x = 100;
+long x = 100;
 
 // stepper
-//AccelStepper stepper1(AccelStepper::DRIVER, 9, 8);
+AccelStepper stepper1(AccelStepper::DRIVER, 8, 9);
 //AccelStepper stepper1(AccelStepper::FULL2WIRE, 9, 8);
-
+//AccelStepper stepper1(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
 
 void setup() {
   lcd.init();  //initialize the lcd
@@ -38,9 +37,16 @@ void setup() {
   //stepper1.setMaxSpeed(200);
   //stepper1.setAcceleration(50);
 
+  //stepper1.setMaxSpeed(2000); //20000 steps/second max
+  //stepper1.setSpeed(63.33);	//19 rpm
+
+  stepper1.setMaxSpeed(200.0);
+  stepper1.setAcceleration(100.0);
+  stepper1.moveTo(0);
+
   // stepper
-  pinMode(9,OUTPUT); // set Pin9 as PUL
-  pinMode(8,OUTPUT); // set Pin8 as DIR
+  //pinMode(9,OUTPUT); // set Pin9 as PUL
+  //pinMode(8,OUTPUT); // set Pin8 as DIR
 
   show();
 
@@ -94,27 +100,28 @@ void loop() {
     show();
     delay(key_delay);
 
-    digitalWrite(8,HIGH); // set high level direction
-    for(x = 0; x < 400; x++) {
-      digitalWrite(9,HIGH); // Output high
-      delayMicroseconds(500); // set rotate speed
-      digitalWrite(9,LOW); // Output low
-      delayMicroseconds(500); // set rotate speed
-    }
-    delay(1000); //pause 1 second
-    digitalWrite(8,LOW); // set high level direction
-    for(x = 0; x < 400; x++) {
-      digitalWrite(9,HIGH);
-      delayMicroseconds(500);
-      digitalWrite(9,LOW);
-      delayMicroseconds(500);
-    }
-    delay(1000);
+    //x += 10000;
+    x = (tooth_num / total_teeth * 360 * 1000);
 
-    //x += 100;
-    //x = (tooth_num / total_teeth * 360);
+	  //delay(1000);
+	  //stepper1.moveTo(x);
+	  //stepper1.setMaxSpeed(200);
+	  //stepper1.setAcceleration(200);
+
+    x = rand() % 200;
+    
+    delay(1000);
+	  stepper1.moveTo(x);
+	  stepper1.setMaxSpeed(200);
+	  stepper1.setAcceleration(200);
+
     //stepper1.moveTo(x);
+    //delay(1000);
+    //stepper1.runSpeedToPosition(); // Blocks until in position
     //stepper1.run();
+    //delay(1000);
   }
+
+  stepper1.run();
 
 } // loop
